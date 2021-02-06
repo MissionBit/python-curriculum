dfb249b36b2297669a38d11c64a5bde9c5173bc3
Yash Shah <yashshah0127@gmail.com>
2021-02-06 22:26:18 +0000

    Add variable mini challenge for week 1

---
+++ week1/variable_mini_challenge.py
@@ -0,0 +1,34 @@
+#Exercise 1
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+#Exercise 2
+
+#Time for a weather report
+weather = "Sunny"
+temperature = "75"
+
+#Print weather
+print("Weather is " + weather + "and" + temperature + "degrees")
+
+#Check what the weather is outside and update weather and temperature
+
+#Print weather
+print("Weather now is " + weather + "and" + temperature + "degrees")
