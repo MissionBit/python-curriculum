b54ba988feb3abc091bfdb564a148d95911f675a
Yash Shah <yashshah0127@gmail.com>
2021-02-06 16:05:27 -0800

    Add loops and dictionaries challenges

---
+++ week3/loops_challenge.py
@@ -0,0 +1,159 @@
+# ----------------------------------------------------------
+# Challenge 1 - Divisible by Ten
+# ----------------------------------------------------------
+# Create a function named divisible_by_ten() that takes a list of numbers named nums as a parameter.
+
+# Return the count of how many numbers in the list are divisible by 10.
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(divisible_by_ten([20, 25, 30, 35, 40]))
+
+# ----------------------------------------------------------
+# Challenge 2 - Greetings
+# ----------------------------------------------------------
+# Create a function named add_greetings() which takes a list of strings named names as a parameter.
+
+# In the function, create an empty list that will contain each greeting. Add the string "Hello, " in front of each name in names and append the greeting to the list.
+
+# Return the new list containing the greetings.
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(add_greetings(["Owen", "Max", "Sophie"]))
+
+# ----------------------------------------------------------
+# Challenge 3 - Delete Starting Even Numbers
+# ----------------------------------------------------------
+
+# Write a function called delete_starting_evens() that has a parameter named lst.
+
+# The function should remove elements from the front of lst until the front of the list is not even. The function should then return lst.
+
+# For example if lst started as [4, 8, 10, 11, 12, 15], then delete_starting_evens(lst) should return [11, 12, 15].
+
+# Make sure your function works even if every element in the list is even!
+
+# Write your function here
+
+
+# Uncomment the lines below when your function is done
+# print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+# print(delete_starting_evens([4, 8, 10]))
+
+# ----------------------------------------------------------
+# Challenge 4 - Odd Indices
+# ----------------------------------------------------------
+
+# Create a function named odd_indices() that has one parameter named lst.
+
+# The function should create a new empty list and add every element from lst that has an odd index. The function should then return this new list.
+
+# For example, odd_indices([4, 3, 7, 10, 11, -2]) should return the list [3, 10, -2].
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(odd_indices([4, 3, 7, 10, 11, -2]))
+
+# ----------------------------------------------------------
+# Challenge 5 - Exponents
+# ----------------------------------------------------------
+
+# Create a function named exponents() that takes two lists as parameters named bases and powers. Return a new list containing every number in bases raised to every number in powers.
+
+# For example, consider the following code.
+
+# exponents([2, 3, 4], [1, 2, 3])
+
+# the result would be the list [2, 4, 8, 3, 9, 27, 4, 16, 64]. It would first add two to the first. Then two to the second. Then two to the third, and so on.
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(exponents([2, 3, 4], [1, 2, 3]))
+
+# ----------------------------------------------------------
+# Challenge 6 - Larger Sum
+# ----------------------------------------------------------
+
+# Create a function named larger_sum() that takes two lists of numbers as parameters named lst1 and lst2.
+
+# The function should return the list whose elements sum to the greater number. If the sum of the elements of each list are equal, return lst1.
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+# ----------------------------------------------------------
+# Challenge 7 - Over 9000
+# ----------------------------------------------------------
+
+# Create a function named over_nine_thousand() that takes a list of numbers named lst as a parameter.
+
+# The function should sum the elements of the list until the sum is greater than 9000. When this happens, the function should return the sum. If the sum of all of the elements is never greater than 9000, the function should return total sum of all the elements. If the list is empty, the function should return 0.
+
+# For example, if lst was [8000, 900, 120, 5000], then the function should return 9020.
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(over_nine_thousand([8000, 900, 120, 5000]))
+
+# ----------------------------------------------------------
+# Challenge 8 - Max Num
+# ----------------------------------------------------------
+
+# Create a function named max_num() that takes a list of numbers named nums as a parameter.
+
+# The function should return the largest number in nums
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(max_num([50, -10, 0, 75, 20]))
+
+# ----------------------------------------------------------
+# Challenge 9 - Same Values
+# ----------------------------------------------------------
+
+# Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+
+# The function should return a list of the indices where the values were equal in lst1 and lst2.
+
+# For example, the following code should return [0, 2, 3]
+
+# same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])
+
+# Write your function here
+
+
+# Uncomment the line below when your function is done
+# print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+# ----------------------------------------------------------
+# Challenge 10 - Reversed List
+# ----------------------------------------------------------
+
+# Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
+
+# The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+
+# For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+
+# Write your function here
+
+
+# Uncomment the lines below when your function is done
+# print(reversed_list([1, 2, 3], [3, 2, 1]))
+# print(reversed_list([1, 5, 3], [3, 2, 1]))
---
+++ week4/dictionaries_challenge.py
@@ -0,0 +1,83 @@
+# ----------------------------------------------------------
+# Challenge 1: Sum Values
+# ----------------------------------------------------------
+
+# Write a function named sum_values that takes a dictionary named my_dictionary as a parameter. The function should return the sum of the values of the dictionary
+
+# Write your sum_values function here:
+
+# Uncomment these function calls to test your sum_values function:
+#print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+#print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
+
+# ----------------------------------------------------------
+# Challenge 2: Even Keys
+# ----------------------------------------------------------
+
+# Create a function called sum_even_keys that takes a dictionary named my_dictionary, with all integer keys and values, as a parameter. This function should return the sum of the values of all even keys.
+
+# Write your sum_even_keys function here:
+
+# Uncomment these function calls to test your  function:
+#print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+#print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6
+
+# ----------------------------------------------------------
+# Challenge 3: Add Ten
+# ----------------------------------------------------------
+
+# Create a function named add_ten that takes a dictionary with integer values named my_dictionary as a parameter. The function should add 10 to every value in my_dictionary and return my_dictionary
+
+# Write your add_ten function here:
+
+# Uncomment these function calls to test your  function:
+#print(add_ten({1:5, 2:2, 3:3}))
+# should print {1:15, 2:12, 3:13}
+#print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
+
+# ----------------------------------------------------------
+# Challenge 4: Values That Are Keys
+# ----------------------------------------------------------
+
+# Create a function named values_that_are_keys that takes a dictionary named my_dictionary as a parameter. This function should return a list of all values in the dictionary that are also keys.
+
+# Write your values_that_are_keys function here:
+
+# Uncomment these function calls to test your  function:
+#print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+#print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
+
+# ----------------------------------------------------------
+# Challenge 5: Largest Value
+# ----------------------------------------------------------
+
+# Write a function named max_key that takes a dictionary named my_dictionary as a parameter. The function should return the key associated with the largest value in the dictionary.
+
+# Write your max_key function here:
+
+# Uncomment these function calls to test your  function:
+#print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+#print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
+
+# ----------------------------------------------------------
+# Challenge 6: Word Length Dict
+# ----------------------------------------------------------
+
+# Write a function named word_length_dictionary that takes a list of strings named words as a parameter. The function should return a dictionary of key/value pairs where every key is a word in words and every value is the length of that word.
+
+# Write your word_length_dictionary function here:
+
+# Uncomment these function calls to test your  function:
+#print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+#print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
