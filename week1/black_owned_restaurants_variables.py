b1bf29bdee0d967ad3558c8db3b497e8feea8178
Yash Shah <yashshah0127@gmail.com>
2021-02-07 20:29:18 -0800

    Add week 1 and 2 exercises for the black-owned restaurant finder

---
+++ week1/black_owned_restaurants_variables.py
@@ -0,0 +1,23 @@
+# Black-Owned Restaurant Finder: Variables exercises
+
+####################################################################################################
+# Write code that prints out the string "Calculating distances..."
+
+# YOUR CODE HERE
+
+####################################################################################################
+# Write code that asks the user if they would like takeout or delivery, then prints out what the
+# user chose (e.g. "You chose takeout" or "You chose delivery"). Use a variable to store the user's
+# choice.
+
+# YOUR CODE HERE
+
+####################################################################################################
+# Find the name of a black-owned restaurant nearby and store it in a variable. Then, find out how
+# far away it is (in miles) and store that in another variable. Finally, use those variables to
+# print a message that says something like "The restaurant Bella Trattoria is 1.3 miles away."
+
+# TIP: Use Google Maps to find nearby black-owned restaurants
+# https://www.google.com/maps/search/black+owned+restaurants
+
+# YOUR CODE HERE
---
+++ week2/black_owned_restaurants_conditionals.py
@@ -0,0 +1,9 @@
+# Black-Owned Restaurant Finder: Conditionals exercises
+
+####################################################################################################
+# Using your code from the previous "variables" exercise, write code that asks the user if they
+# would like takeout or delivery. If the user did not enter anything, print the string "Please enter
+# a choice." If the user typed in something other than "takeout" or "delivery", print the string
+# "Please choose either 'takeout' or 'delivery'."
+
+# YOUR CODE HERE
