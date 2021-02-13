c3eebd5cb015037a5c5eeab7bb5fe69a91320b0b
Yash Shah <yashshah0127@gmail.com>
2021-02-13 08:17:16 -0800

    Add black owned restaurants finder week 3 exercise

---
+++ week3/black_owned_restaurants_lists.py
@@ -0,0 +1,36 @@
+# Black-Owned Restaurant Finder: Lists exercises
+
+####################################################################################################
+# Think about different cuisines (as many as you want) and store them in a list. Using what you
+# learned in the previous exercises, write code that asks the user what cuisine they are interested
+# in. If the cuisine they entered is not in the list, print the string "Please choose one of these
+# cuisines: [LIST OF CUISINES HERE]".
+
+# YOUR CODE HERE
+
+####################################################################################################
+# Find the name of at least three black-owned restaurants nearby and store their names in a list.
+# Then, print out the name of each restaurant, one line at a time.
+
+# TIP: Use Google Maps to find nearby black-owned restaurants
+# https://www.google.com/maps/search/black+owned+restaurants
+
+# YOUR CODE HERE
+
+####################################################################################################
+# Take your list of restaurants from the previous challenge and print them out in a numbered list.
+# Example:
+# 1. <RESTAURANT 1>
+# 2. <RESTAURANT 2>
+# 3. <RESTAURANT 3>
+
+# YOUR CODE HERE
+
+####################################################################################################
+# Find out how far away each restaurant is (in miles) and store those distances in another list.
+# Then, print out the name of each restaurant AND how far away it is in a numbered list. Example:
+# 1. <RESTAURANT 1> is 2.4 miles away
+# 2. <RESTAURANT 2> is 6.8 miles away
+# 3. <RESTAURANT 3> is 10.2 miles away
+
+# YOUR CODE HERE
