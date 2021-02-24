e2f1c06d1247b17a71eda7f6bebaeca83a45e78f
missionbit <info@missionbit.com>
2021-02-24 05:03:02 +0000

    Review activities week 3 and 4

---
+++ week3/week3_review.py
@@ -0,0 +1,32 @@
+#Let's review variables and conditionals!
+
+#TASK: Using conditionals and user input, tell the user what a good career path would be. 
+#The user should be asked THREE questions. Use conditionals to check the answers to the questions. Ask a different question depending on the answer and after three questions print out a response.
+
+#Example 1:
+
+#ASK: Do you like science? 
+#USER ANSWER: Yes
+
+#ASK: Do you like to be on your feet or work at a desk?
+#USER ANSWER: Work at a desk
+
+#ASK: Do you like collaborating with others or working alone?
+#USER ANSWER: Collaborating
+
+#PRINT: You should be a computer programmer!
+
+#Example 2:
+
+#ASK: Do you like science? 
+#USER ANSWER: No
+
+#ASK: Are you more analytical or creative?
+#USER ANSWER: Creative
+
+#ASK: Do you like writing?
+#USER ANSWER: Yes
+
+#Write your code here:
+
+#PRINT: You should be an author!
---
+++ week4/week4_review.py
@@ -0,0 +1,10 @@
+#Let's review conditionals and lists!
+
+#TASK: Create a list of chores corresponding to days of the week. Your list should be of length 7 for each day of the week and include one chore on each day. Ask the user an input question to get the current day of the week. 
+#Using conditionals, return the chore in the list corresponding to the day from the user input. For example, if the user inputs "Tuesday", return the 3rd chore in the list since Tuesday is the 3rd day of the week.
+
+#Example 1:
+
+#LIST OF CHORES: Clean room, Homework, Study Python, Homework, Volunteer, Laundry, Plan next week
+#USER INPUT: Tuesday
+#PRINT: Study Python
