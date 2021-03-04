4e47847c6a308d80da8aa2c8977489288bfa87be
Yash Shah <yashshah0127@gmail.com>
2021-03-04 04:06:43 +0000

    Activity updates

--- week4/dictionaries_challenge.py
+++ week4/dictionaries_challenge.py
@@ -8,11 +8,59 @@
 
 
 
+
+#----------------------------------------------------------
+# Challenge 2: Max Value
+#----------------------------------------------------------
+
+# Create a dictionary called groceries with items as the keys and prices as the values. Print the highest price of a grocery.
+
+# Write your code here:
+
+
+
+
+#----------------------------------------------------------
+# Challenge 3: Grouping food
+#----------------------------------------------------------
+
+# Create a dictionary called groceries with the name of the grocery as the keys and the food group as the value. Print each food group with the number of items in the food group.
+
+# Write your code here:
+
+
+
+
+#----------------------------------------------------------
+# Challenge 4: Updating values
+#----------------------------------------------------------
+
+# Create a dictionary called groceries with items as the keys and prices as the values. Compute the average price of all of the foods, and update the price of all of the food keys to now be equal to the average price.
+
+# Write your code here:
+
+
+
+# should print 10
+
+
+
+
+#----------------------------------------------------------
+# Challenge 5: List values
+#----------------------------------------------------------
+
+# Create a dictionary called groceries with the name of the grocery as the key and a list of the stores it is available at as the value. Print out the store that the most amount of foods are available at.
+
+# Write your code here:
+
+
+
 # should print 10
 
 
 #----------------------------------------------------------
-# Challenge 2: Even Keys
+# Challenge 6: Even Keys
 #----------------------------------------------------------
 
 # Create a dictionary named ints, with all integer keys and values. Print the sum of the values of all even keys.
@@ -24,7 +72,7 @@
 
 
 #----------------------------------------------------------
-# Challenge 3: Add Ten
+# Challenge 7: Add Ten
 #----------------------------------------------------------
 
 # Create a dictionary with integer values named integers. Add 10 to every value in my_dictionary and print my_dictionary
@@ -38,7 +86,7 @@
 
 
 #----------------------------------------------------------
-# Challenge 4: Largest Value
+# Challenge 8: Largest Value
 #----------------------------------------------------------
 
 # Create a dictionary named max_nums with all integer values. Print the key associated with the largest value in the dictionary.
@@ -49,7 +97,7 @@
 # should print "c"
 
 #----------------------------------------------------------
-# Challenge 5: Word Length Dict
+# Challenge 9: Word Length Dict
 #----------------------------------------------------------
 
 # Create a list of strings named words. Print a dictionary of key/value pairs where every key is a word in words and every value is the length of that word.
@@ -61,7 +109,7 @@
 
 
 #----------------------------------------------------------
-# Challenge 6: Contact Data
+# Challenge 10: Contact Data
 #----------------------------------------------------------
 
 #Create a dictionary called contact_data that stores a list of contact data for your friends and family. This dictionary should include name as the key and have phone number, email address and home address in the value. Print out the email of every person in the dictionary.
