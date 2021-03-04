f55d822c88850b4ad5ee893e3db5ca5e7b219e94
Yash Shah <yashshah0127@gmail.com>
2021-03-03 19:49:44 -0800

    Add black owned restaurants finder week 4 exercises

---
+++ week4/black_owned_restaurants_dictionaries.py
@@ -0,0 +1,21 @@
+# Black-Owned Restaurant Finder: Dictionaries exercises
+
+############################################ Exercise 1 ############################################
+# Find the name of at least three black-owned restaurants nearby, and find out how far away they are
+# from you in miles. Store the names of the restaurants and their distances in a dictionary, with
+# the restaurant names as the keys and the distances as the values. Using the dictionary, print out
+# the name of each restaurant and how far away it is, with each restaurant on its own line. Example:
+# <RESTAURANT 1> is 6.8 miles away
+# <RESTAURANT 2> is 2.4 miles away
+# <RESTAURANT 3> is 10.2 miles away
+
+# TIP: Use Google Maps to find nearby black-owned restaurants
+# https://www.google.com/maps/search/black+owned+restaurants
+
+# YOUR CODE HERE
+
+############################################ Exercise 2 ############################################
+# Using the dictionary you created in the previous exercise, find the name of the closest restaurant
+# and print out a message such as "<RESTAURANT 1> is the closest restaurant".
+
+# YOUR CODE HERE
