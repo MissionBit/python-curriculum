984d2440ba7179a325c2611b1dabf20aea0788a1
Yash Shah <yashshah0127@gmail.com>
2021-03-25 22:29:26 +0000

    Week 7 review

---
+++ week7/week7_review.py
@@ -0,0 +1,24 @@
+#Let's review functions!
+
+#TASK: It's time for you to sign up for classes! You have 5 slots during the day. Using the dictionary of class availablity and the schedule list below, create a function that will you up for a class. You can only sign up if the time slot of the class is still available on the your calendar. Then call your function enough times to fill your calendar. Then print out your schedule.
+
+
+#This dictionary has the class as a key, and the value is the time slot.
+classes = {
+  "Chemistry" : 4, 
+  "Math" : 2,
+  "Computer Science" : 3,
+  "History" : 3,
+  "English" : 1,
+  "PE" : 0,
+  "Band" : 0,
+  "Physics" : 4,
+  "Biology" : 4}
+
+my_schedule = []
+
+
+#Example end result:
+my_schedule = ['Band', 'English', 'Math', 'Computer Science', 'Chemistry']
+
+
