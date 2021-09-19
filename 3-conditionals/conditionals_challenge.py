c16837892f70efdb66a6a27308523fd1b427dfd5
Yash Shah <yashshah0127@gmail.com>
2021-09-19 12:18:27 -0700

    Involve functions in conditionals challenge

--- 3-conditionals/conditionals_challenge.py
+++ 3-conditionals/conditionals_challenge.py
@@ -1,49 +1,73 @@
 # ----------------------------------------------------------
 # Challenge 1 - Greater than 10
 # ----------------------------------------------------------
-# Write python that initializes a variable called num to 4 and prints "True" if it is greater than
-# 10 and "False" if it is less than 10.  Make sure that is prints out false since num is set to 4.
-# Change num to some other values and see what happens!
+# Write a function that accepts a parameter called `num` and prints "True" if it is greater than 10
+# and "False" if it is less than or equal to 10. Make sure that it prints out False if num is set to
+# 4. Change num to some other values and see what happens!
 
-# Write your code here
+def greater_than_ten(num):
+  # REPLACE "pass" WITH YOUR CODE
+  pass
+
+greater_than_ten(4)
+greater_than_ten(15)
 
 # ----------------------------------------------------------
 # Challenge 2 - Doubled greater than 10
 # ----------------------------------------------------------
-# Write python that initializes a variable called num to 4 and prints "True" if 2 times num is
-# greater than 10 and "False" if it is less than 10. Make sure that is prints out false since num is
-# set to 4. Change num to some other values and see what happens!
+# Write a function that accepts a parameter called `num` prints "True" if 2 times num is greater
+# than 10 and "False" if it is less than or equal to 10. Make sure that it prints out False if num
+# is set to 4. Change num to some other values and see what happens!
 
-# Write your code here
+def twice_greater_than_ten(num):
+  # REPLACE "pass" WITH YOUR CODE
+  pass
+
+twice_greater_than_ten(4)
+twice_greater_than_ten(6)
 
 # ----------------------------------------------------------
 # Challenge 3 - Even or Odd
 # ----------------------------------------------------------
-# Write python that initializes a variable called num to 4 and prints "Odd" if it is odd and "Even"
-# if it is even. Make sure that is prints out even since num is set to 4. Change num to some other
-# values and see what happens!
+# Write a function that accepts a parameter called `num` prints "Odd" if it is odd and "Even" if it
+# is even. Make sure that it prints out Even if num is set to 4. Change num to some other values and
+# see what happens!
+# HINT: use the modulo operator (%)
 
-# Hint: use mod (%)
+def odd_or_even(num):
+  # REPLACE "pass" WITH YOUR CODE
+  pass
 
-# Write your code here
+odd_or_even(4)
+odd_or_even(3)
 
 # ----------------------------------------------------------
 # Challenge 4 - Favorite Color
 # ----------------------------------------------------------
-# You want to see if you and your best friend have the same favorite color. Initialize a variable
-# called "my_fav_color" and have them initialize one called "friend_fav_color". If they are the
-# same, print "We have the same favorite color!" Else, print a message of sadness
+# You want to see if you and your best friend have the same favorite color. Write a function that
+# accepts two parameters called "my_fav_color" and "friend_fav_color". If they are the same, print
+# "We have the same favorite color!" Else, print a message of sadness. Try out different values to
+# see if it works!
 
-# Write your code here
+def same_color(my_fav_color, friend_fav_color):
+  # REPLACE "pass" WITH YOUR CODE
+  pass
+
+same_color("Blue", "Green")
+same_color("Blue", "Blue")
 
 # ----------------------------------------------------------
 # Challenge 5 - Favorite Food Type
 # ----------------------------------------------------------
 # Pick your favorite food from this list: Pizza, Salad, Steak, Strawberries, Ice cream, Cheese,
-# Pasta,  Carrots, Apples. Initialize a variable named food to be that food. Then using conditionals
-# using the list of food we have, print out what food group the food is from. Your code should still
-# work when you change the food variable.
+# Pasta, Carrots, Apples. Write a function that accepts a parameter called `food`. Using the list of
+# food and conditionals, print out what food group the food is from. Your code should still work
+# for different `food` values.
+# HINT: use `elif`
 
-# Hint: use elif
+def food_group(food):
+  # REPLACE "pass" WITH YOUR CODE
+  pass
 
-# Write your code here
+food_group("Pasta")
+food_group("Carrots")
