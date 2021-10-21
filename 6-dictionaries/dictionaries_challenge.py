97dcd52ce2770f97b009253ddf9c5d28312666de
Yash Shah <yashshah0127@gmail.com>
2021-10-21 10:36:04 -0700

    Clean up dictionaries challenge

--- 6-dictionaries/dictionaries_challenge.py
+++ 6-dictionaries/dictionaries_challenge.py
@@ -2,119 +2,143 @@
 # Challenge 1: Sum Values
 #----------------------------------------------------------
 
-# Create a dictionary called groceries. Print the sum of the values of the dictionary
+# Write a function named sum_values that takes a dictionary named my_dictionary as a parameter. The
+# function should return the sum of the values of the dictionary.
 
-# Write your code here:
+# Write your sum_values function here:
 
-
-
-
-#----------------------------------------------------------
-# Challenge 2: Max Value
-#----------------------------------------------------------
-
-# Create a dictionary called groceries with items as the keys and prices as the values. Print the highest price of a grocery.
-
-# Write your code here:
-
-
-
-
-#----------------------------------------------------------
-# Challenge 3: Grouping food
-#----------------------------------------------------------
-
-# Create a dictionary called groceries with the name of the grocery as the keys and the food group as the value. Print each food group with the number of items in the food group.
-
-# Write your code here:
-
-
-
-
-#----------------------------------------------------------
-# Challenge 4: Updating values
-#----------------------------------------------------------
-
-# Create a dictionary called groceries with items as the keys and prices as the values. Compute the average price of all of the foods, and update the price of all of the food keys to now be equal to the average price.
-
-# Write your code here:
-
-
-
+# Uncomment these function calls to test your sum_values function:
+# print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
-
-
-
-
-#----------------------------------------------------------
-# Challenge 5: List values
-#----------------------------------------------------------
-
-# Create a dictionary called groceries with the name of the grocery as the key and a list of the stores it is available at as the value. Print out the store that the most amount of foods are available at.
-
-# Write your code here:
-
-
-
-# should print 10
-
+# print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
 
 #----------------------------------------------------------
-# Challenge 6: Even Keys
+# Challenge 2: Even Keys
 #----------------------------------------------------------
 
-# Create a dictionary named ints, with all integer keys and values. Print the sum of the values of all even keys.
+# Create a function called sum_even_keys that takes a dictionary named my_dictionary, with all
+# integer keys and values, as a parameter. This function should return the sum of the values of all
+# even keys.
 
-# Write your code here:
+# Write your sum_even_keys function here:
 
-
+# Uncomment these function calls to test your function:
+# print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
-
+# print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6
 
 #----------------------------------------------------------
-# Challenge 7: Add Ten
+# Challenge 3: Add Ten
 #----------------------------------------------------------
 
-# Create a dictionary with integer values named integers. Add 10 to every value in my_dictionary and print my_dictionary
+# Create a function named add_ten that takes a dictionary with integer values named my_dictionary as
+# a parameter. The function should add 10 to every value in my_dictionary and return my_dictionary.
 
-# Write your code here:
+# Write your add_ten function here:
 
-
+# Uncomment these function calls to test your function:
+# print(add_ten({1:5, 2:2, 3:3}))
 # should print {1:15, 2:12, 3:13}
-
-
-
+# print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
 
 #----------------------------------------------------------
-# Challenge 8: Largest Value
+# Challenge 4: Values That Are Keys
 #----------------------------------------------------------
 
-# Create a dictionary named max_nums with all integer values. Print the key associated with the largest value in the dictionary.
+# Create a function named values_that_are_keys that takes a dictionary named my_dictionary as a
+# parameter. This function should return a list of all values in the dictionary that are also keys.
 
-# Write your code here:
+# Write your values_that_are_keys function here:
 
+# Uncomment these function calls to test your function:
+# print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+# print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
 
+#----------------------------------------------------------
+# Challenge 5: Largest Value
+#----------------------------------------------------------
+
+# Write a function named max_key that takes a dictionary named my_dictionary as a parameter. The
+# function should return the key associated with the largest value in the dictionary.
+
+# Write your max_key function here:
+
+# Uncomment these function calls to test your function:
+# print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+# print(max_key({"a":100, "b":10, "c":1000}))
 # should print "c"
 
 #----------------------------------------------------------
-# Challenge 9: Word Length Dict
+# Challenge 6: Word Length Dict
 #----------------------------------------------------------
 
-# Create a list of strings named words. Print a dictionary of key/value pairs where every key is a word in words and every value is the length of that word.
+# Write a function named word_length_dictionary that takes a list of strings named words as a
+# parameter. The function should return a dictionary of key/value pairs where every key is a word in
+# words and every value is the length of that word.
+
+# Write your word_length_dictionary function here:
+
+# Uncomment these function calls to test your function:
+# print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+# print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
+
+#----------------------------------------------------------
+# Challenge 7: Max Value
+#----------------------------------------------------------
+
+# Create a dictionary called groceries with items as the keys and prices as the values. Print the
+# highest price of a grocery.
 
 # Write your code here:
 
 
-# should print {"apple":5, "dog": 3, "cat":3}
+#----------------------------------------------------------
+# Challenge 8: Grouping food
+#----------------------------------------------------------
+
+# Create a dictionary called groceries with the name of the grocery as the keys and the food group
+# as the value. Print each food group with the number of items in the food group.
+
+# Write your code here:
 
 
 #----------------------------------------------------------
-# Challenge 10: Contact Data
+# Challenge 9: Updating values
 #----------------------------------------------------------
 
-#Create a dictionary called contact_data that stores a list of contact data for your friends and family. This dictionary should include name as the key and have phone number, email address and home address in the value. Print out the email of every person in the dictionary.
+# Create a dictionary called groceries with items as the keys and prices as the values. Compute the
+# average price of all of the foods, and update the price of all of the food keys to now be equal to
+# the average price.
+
+# Write your code here:
+
+
+#----------------------------------------------------------
+# Challenge 10: List values
+#----------------------------------------------------------
+
+# Create a dictionary called groceries with the name of the grocery as the key and a list of the
+# stores it is available at as the value. Print out the store that the most amount of foods are
+# available at.
+
+# Write your code here:
+
+
+#----------------------------------------------------------
+# Challenge 11: Contact Data
+#----------------------------------------------------------
+
+# Create a dictionary called contact_data that stores a list of contact data for your friends and
+# family. This dictionary should include name as the key and have phone number, email address and
+# home address in the value. Print out the email of every person in the dictionary.
 
 #Write your code here:
 
-
-#should print out all email addresses of contacts
